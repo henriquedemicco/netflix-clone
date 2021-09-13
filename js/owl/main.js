e4bd3a0f47472ca291929a86b61a34609e772f10
Miccao <henriquedemicco@hotmail.com>
2021-09-13 20:42:17 -0300

    Primeiro commit

---
+++ js/owl/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        380:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
